f5d025a08a729b9790925bccce38daa5b95fb378
ralhassan <ralhassan@site.sa>
2020-10-16 14:04:22 +0300

    adding database URI to config.py file

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -9,5 +9,5 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# TODO: connect to a local postgresql database (done)
+SQLALCHEMY_DATABASE_URI = 'postgresql://reema@localhost:5432/fyyur'
