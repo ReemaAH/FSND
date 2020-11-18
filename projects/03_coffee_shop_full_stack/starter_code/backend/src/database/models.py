e08820e25932567e9840251f4b950c3d0624dcd5
ralhassan <ralhassan@site.sa>
2020-11-18 18:52:59 +0300

    using postgresql

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
@@ -2,10 +2,10 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate
 
-database_filename = "database.db"
-project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_name = "coffeeshop"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -18,6 +18,7 @@ def setup_db(app):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    migrate = Migrate(app, db)
 
 '''
 db_drop_and_create_all()
